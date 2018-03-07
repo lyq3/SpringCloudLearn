6b0b04271f573de9dddcda89d31a8a7b103351d1
罗云强 <99190092@qq.com>
2018-03-07 22:18:32 +0800

    版本真蛋疼

--- server1/src/main/java/com/lyq3/server1/EurekaserverApplication.java
+++ server1/src/main/java/com/lyq3/server1/EurekaserverApplication.java
@@ -2,6 +2,7 @@ package com.lyq3.server1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
