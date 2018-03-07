ad1ea58cf6585b6dae9ee6f20c204e498c419b27
罗云强 <99190092@qq.com>
2018-03-07 22:50:53 +0800

    ribbon

--- client1/src/main/java/com/lyq3/client1/TestController.java
+++ client1/src/main/java/com/lyq3/client1/TestController.java
@@ -1,12 +1,15 @@
 package com.lyq3.client1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${server.port}")
+    private String port;
     @RequestMapping("/test")
     public String test1(){
-        return "123123213";
+        return port;
     }
 }
---
+++ ribbon1/src/main/java/com/lyq3/ribbon1/HelloController.java
@@ -0,0 +1,15 @@
+package com.lyq3.ribbon1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    private HelloService service;
+    @RequestMapping("/hello")
+    public String test2(){
+        return service.hiService();
+    }
+}
