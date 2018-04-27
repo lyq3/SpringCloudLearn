66fa7f25aab9da49824882eab0f4165122356f1f
罗云强 <99190092@qq.com>
2018-04-27 22:22:49 +0800

    聚合监控没调试成功

---
+++ turbine/src/main/java/com/lyq3/turbine/TurbineApplication.java
@@ -0,0 +1,20 @@
+package com.lyq3.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 卡卢比
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableTurbine
+@EnableDiscoveryClient
+public class TurbineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class,args);
+    }
+}
