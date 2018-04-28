76355028cc868ec1a929dc859502360031d563f3
lyq3 <99190092@qq.com>
2018-04-28 11:32:20 +0800

    MQ

---
+++ stream-mq/src/main/java/com/lyq3/StreamApplication.java
@@ -0,0 +1,14 @@
+package com.lyq3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 卡卢比
+ */
+@SpringBootApplication
+public class StreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApplication.class,args);
+    }
+}
